fb8e239a150e741ccdc3176c23a08800d820c59a
Kyle Jorve <kyle.jorve@gmail.com>
2022-12-08 19:15:03 -0800

    some fixes

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,9 @@
+import "../styles/base/_index.css";
 import type { AppProps } from "next/app";
 import { SiteContextProvider } from "../context/global";
 import Layout from "../components/layout/Layout";
 import DefaultHead from "../components/layout/DefaultHead";
 import Script from "next/script";
-import "../styles/base/_index.css";
 
 export default function App({ Component, pageProps }: AppProps) {
     return (
