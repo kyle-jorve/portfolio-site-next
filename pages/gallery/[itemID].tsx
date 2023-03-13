6b1e58bd9d4b39db31f08033029931eec6b25ad2
Kyle Jorve <34990609+kyle-jorve@users.noreply.github.com>
2023-03-13 16:34:36 -0700

    progress

--- pages/gallery/[itemID].tsx
+++ pages/gallery/[itemID].tsx
@@ -1,17 +1,15 @@
-import { Fragment } from 'react';
-import Head from 'next/head';
-import { useRouter } from 'next/router';
-import useGalleryData from '../../data/gallery-data';
-import MediaShowcase from '../../components/gallery/detail/MediaShowcase';
-import ContentShowcase from '../../components/gallery/detail/ContentShowcase';
-import Custom404 from '../404';
-import styles from '../../styles/components/Showcase.module.css';
+import { Fragment } from "react";
+import { useRouter } from "next/router";
+import { items as galleryItems } from "../../data/gallery-data";
+import Head from "next/head";
+import Custom404 from "../404";
+import Button from "../../components/layout/Button";
+import styles from "../../styles/components/Showcase.module.css";
 
 export default function GalleryDetail() {
-	const galleryData = useGalleryData();
 	const router = useRouter();
 	const itemID = router.query.itemID;
-	const galleryItem = galleryData.items.find((item) => item.name === itemID);
+	const galleryItem = galleryItems.find((item) => item.name === itemID);
 
 	if (!galleryItem) {
 		return <Custom404 />;
@@ -20,21 +18,23 @@ export default function GalleryDetail() {
 	return (
 		<Fragment>
 			<Head>
-				<title key="title">
-					{galleryItem.title} by Kyle Jorve | Illustration and Design
-				</title>
+				<title key="title">{galleryItem.title} by Kyle Jorve | Illustration and Design</title>
 				<meta
 					property="og:image"
-					content={`https://kylejorve.com${galleryItem.thumbnailKey.path}-640.jpg`}
+					content={`https://kylejorve.com${galleryItem.thumb.path}-640.jpg`}
 					key="meta-image"
 				/>
 			</Head>
-			<section className={styles.showcase}>
-				<MediaShowcase item={galleryItem} />
 
-				<ContentShowcase item={galleryItem}>
-					{!!galleryItem?.content && galleryItem.content}
-				</ContentShowcase>
+			<section className={`section swoops swoops--left ${styles.showcase}`}>
+				<Button
+					className="button button--white-border button--back button--small"
+					onClick={() => history.back()}
+				>
+					Back
+				</Button>
+
+				<div className="showcase__grid"></div>
 			</section>
 		</Fragment>
 	);
